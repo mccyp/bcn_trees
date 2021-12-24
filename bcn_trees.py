b88cc9b098370c23b5bb329db3f69d7efbd47057
mccyp <cyprien.lavigne@hotmail.com>
2021-12-24 15:24:00 +0100

    Update bcn_trees.py

--- bcn_trees.py
+++ bcn_trees.py
@@ -4,12 +4,9 @@ import pygsheets
 import sys
 import os
 from flask import Flask, render_template, request, url_for, flash, redirect
-import pandas as pd
 import folium
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def render_index():
