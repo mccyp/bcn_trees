83cbe38aaccc1f12b2c0b3c41ed003430dd0e9d8
mccyp <cyprien.lavigne@hotmail.com>
2021-12-24 15:08:34 +0100

    Add files via upload

--- bcn_trees.py
+++ bcn_trees.py
@@ -1,11 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-Éditeur de Spyder
-
-Ceci est un script temporaire.
-"""
-
-
+import pandas as pd 
+import json
+import pygsheets
+import sys
+import os
 from flask import Flask, render_template, request, url_for, flash, redirect
 import pandas as pd
 import folium
@@ -13,7 +10,7 @@ import folium
 
 app = Flask(__name__)
 
-
+gc = pygsheets.authorize(client_secret='/home/k4y4k/credentials/code_secret.json')
 
 @app.route('/')
 def render_index():
@@ -23,17 +20,44 @@ def render_index():
 
 @app.route('/mapa', methods=(['POST']))
 def render_the_map():
-    content = request.form['text']
-    content = content.lower() 
+    content = request.form.get('content',type=int)
     if not content:
-        flash('''Has d'entrar un titol!''')
+        flash('''Merci de saisir un nombre correspondant à un item !''')
     else:
-        trees = pd.read_csv('arbres_bcn.csv')
-        trees2 = trees.loc[trees['nom_cientific'].str.lower().str.contains(content)]
-        bcn = folium.Map(location = [41.390205, 2.154007], zoom_start = 13)
-        for index, row in trees2.iterrows():
-            folium.Marker(
-                location=[row['latitud'], row['longitud']],
-                popup = row['nom_cientific'] + '\n' + 'Planted on: ' + str(row['data_plantacio']),
-                icon=folium.Icon(color="green", icon='fa-tree', prefix='fa')).add_to(bcn)
-        return bcn._repr_html_()
+        # Open the spreadsheet and the first sheet.
+        sh = gc.open_by_key('1aCzOeWrq8BQFz-39dC07QQ8TOJeT5fNbNSCCh6jLLUY')
+        wks = sh.sheet1
+        df = pd.DataFrame(wks.get_all_values())
+        print(df.iloc[0])
+        cols = df.iloc[0]
+        df.columns = cols
+        del cols[0]
+        df = df.drop(0)      
+        def verdict(df, x):
+            y = int(x)-1
+            row = df.iloc[y]
+            print('''
+                    __,__
+           .--.  .-"     "-.  .--.
+          / .. \/  .-. .-.  \/ .. \\
+         | |  '|  /   Y   \  |'  | |
+         | \   \  \ 0 | 0 /  /   / |
+          \ '- ,\.-"`` ``"-./, -' /
+           `'-' /_   ^ ^   _\ '-'`
+               |  \._   _./  |
+               \   \ `~` /   /
+                '._ '-=-' _.'
+                   '~---~ ''')
+            print('\n||||||||| ITEM N°' + row[0] + ' ||||||||| ')
+            for col in cols:
+                print(col)
+                if( (len(str(col)) > 3) & (len(row[col]) > 2) ):   
+                    print(col)
+                    print('   -> ' + row[col])
+
+        result = verdict(df, sys.argv[1])
+        print(result)
+
+
+
+
